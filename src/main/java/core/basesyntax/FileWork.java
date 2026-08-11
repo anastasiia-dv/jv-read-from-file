0f5b0e448c3a3278b3b01873107bc94ae3272ad7
anastasiia-dv <drozdenko.av08@gmail.com>
2026-08-12 02:11:32 +0300

    implemented readFromFile method. fixed checkstyle.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,19 +1,21 @@
 package core.basesyntax;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class FileWork {
     private static final String SPECIAL_REGEX = "\\W+";
+
     public String[] readFromFile(String fileName) {
         File file = new File(fileName);
         List<String> text;
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
-             text = bufferedReader.readAllLines();
+            text = bufferedReader.readAllLines();
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
