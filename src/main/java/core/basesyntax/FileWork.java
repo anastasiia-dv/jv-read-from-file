9d21dc5fa080106c27562464f57e5fafac73e7a0
anastasiia-dv <drozdenko.av08@gmail.com>
2026-08-12 02:05:53 +0300

    implemented readFromFile method.

--- src/main/java/core/basesyntax/FileWork.java
+++ src/main/java/core/basesyntax/FileWork.java
@@ -1,8 +1,32 @@
 package core.basesyntax;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class FileWork {
+    private static final String SPECIAL_REGEX = "\\W+";
     public String[] readFromFile(String fileName) {
-        //write your code here
-        return null;
+        File file = new File(fileName);
+        List<String> text;
+        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
+             text = bufferedReader.readAllLines();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        List<String> data = new ArrayList<>();
+        for (String line: text) {
+            String[] arrLine = line.toLowerCase().split(SPECIAL_REGEX);
+            for (String word : arrLine) {
+                if (word.startsWith("w")) {
+                    data.add(word);
+                }
+            }
+        }
+        Collections.sort(data);
+        return data.toArray(new String[0]);
     }
 }
